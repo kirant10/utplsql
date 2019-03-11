e4f6bf725eb5ba9f18e9771d06ef3c8d7feace5d
kirant10 <45229103+kirant10@users.noreply.github.com>
2019-03-11 11:46:31 +0530

    Update filr_for_git.sql

--- filr_for_git.sql
+++ filr_for_git.sql
@@ -23,7 +23,7 @@ create or replace package test_betwnstr_git as
   
    -- %test(Returns null for null input string value)
   procedure null_string;
-
+/*
   -- %test(A demo of test raising runtime exception)
   procedure bad_params;
 
@@ -33,7 +33,7 @@ create or replace package test_betwnstr_git as
   -- %test(Demo of a disabled test)
   -- %disabled
   procedure disabled_test;
-
+*/
  
 end;
 /
